ac106a6103cac6e524cae4f039cf5352845ecc8c
rminor2 <146909693+rminor2@users.noreply.github.com>
2024-01-22 17:18:38 -0700

    Update db.go

--- pkg/database/db.go
+++ pkg/database/db.go
@@ -8,23 +8,27 @@ import (
 )
 
 func ConnectToDatabase() (*sql.DB, error) {
-	// Replace with your actual database credentials
 	const (
-		host     = "your-database-host.amazonaws.com"
+		host     = "shootdb-1.czm0a6c2szh6.us-east-2.rds.amazonaws.com"
 		port     = 5432
-		user     = "yourDatabaseUser"
-		password = "yourDatabasePassword"
-		dbname   = "yourDatabaseName"
+		user     = "postgres"
+		password = "Dragon123"
+		dbname   = "shootdb"
 	)
 
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		return nil, err
 	}
+
 	err = db.Ping()
 	if err != nil {
 		return nil, err
 	}
+
+	fmt.Println("Successfully connected!")
 	return db, nil
 }
